56fba8594af825d816271c24009f94c774cdd472
Rishav <rishavloomba@Rishavs-MacBook-Air.local>
2018-09-26 14:28:03 +0530

    first commit

---
+++ thread2.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class thread2 extends Thread
+{
+public void run()
+{
+for(int i=0;i<5;i++)
+{
+try{Thread.sleep(500);}
+catch(InterruptedException e){
+System.out.println(e);
+}
+System.out.println(i);
+}
+}
+public static void main(String[] args)
+{
+thread2 th=new thread2();
+th.start();
+}
+}
